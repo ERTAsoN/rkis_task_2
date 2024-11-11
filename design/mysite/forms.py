96358e404e5179abd2be766e6cc3d1e2cd48ba58
savel_andr <savel.andr.ivanov@gmail.com>
2024-11-11 12:56:17 +0700

    phone_mask

--- design/mysite/forms.py
+++ design/mysite/forms.py
@@ -6,6 +6,7 @@ import re
 class RegistrationForm(forms.ModelForm):
     username = forms.CharField(required=True, max_length=200, label='Имя пользователя', widget=forms.TextInput())
     email = forms.CharField(required=True, max_length=200, label='Email', widget=forms.TextInput())
+    phone = forms.CharField(required=True, max_length=20, label='Телефон', widget=forms.TextInput())
     last_name = forms.CharField(required=True, max_length=200, label='Фамилия', widget=forms.TextInput())
     first_name = forms.CharField(required=True, max_length=200, label='Имя', widget=forms.TextInput())
     patronymic = forms.CharField(max_length=200, label='Отчество (если есть)',  widget=forms.TextInput())
@@ -15,31 +16,31 @@ class RegistrationForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'last_name', 'first_name', 'patronymic', 'password']
+        fields = ['username', 'email', 'phone', 'last_name', 'first_name', 'patronymic', 'password']
 
     def clean_username(self):
         username = self.cleaned_data.get('username')
         if User.objects.filter(username=username).exists():
             raise ValidationError('Данное имя пользователя уже занято.')
-        if not re.match(r'^[a-zA-Z-]+$', username):
+        if not re.match(r'^[A-z-]+$', username):
             raise ValidationError('Имя пользователя должно содержать только латиницу и дефисы.')
         return username
 
     def clean_first_name(self):
         first_name = self.cleaned_data.get('first_name')
-        if not re.match(r'^[а-яА-ЯёЁ\s-]+$', first_name):
+        if not re.match(r'^[А-яёЁ\s-]+$', first_name):
             raise ValidationError('ФИО должно содержать только кириллицу, пробелы и дефисы.')
         return first_name
 
     def clean_last_name(self):
         last_name = self.cleaned_data.get('last_name')
-        if not re.match(r'^[а-яА-ЯёЁ\s-]+$', last_name):
+        if not re.match(r'^[А-яёЁ\s-]+$', last_name):
             raise ValidationError('ФИО должно содержать только кириллицу, пробелы и дефисы.')
         return last_name
 
     def clean_patronymic(self):
         patronymic = self.cleaned_data.get('patronymic')
-        if not re.match(r'^[а-яА-ЯёЁ\s-]+$', patronymic):
+        if not re.match(r'^[А-яёЁ\s-]+$', patronymic):
             raise ValidationError('ФИО должно содержать только кириллицу, пробелы и дефисы.')
         return patronymic
 
--- design/mysite/models.py
+++ design/mysite/models.py
@@ -9,6 +9,7 @@ class User(AbstractUser):
     patronymic = models.CharField(max_length=200, verbose_name='Отчество')
     email = models.EmailField(max_length=200, verbose_name='Email', unique=True)
     password = models.CharField(max_length=200, verbose_name='Пароль')
+    phone = models.CharField(max_length=20, verbose_name='Телефон', default='0000000000')
 
     USERNAME_FIELD = 'username'
 
