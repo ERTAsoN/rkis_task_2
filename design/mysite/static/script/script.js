59339f00e69a62f964a032a9794c8847944154f7
savel_andr <savel.andr.ivanov@gmail.com>
2024-11-12 09:47:33 +0700

    phone_mask

--- design/mysite/static/script/script.js
+++ design/mysite/static/script/script.js
@@ -1,36 +1,87 @@
-window.addEventListener("DOMContentLoaded", function() {
-    [].forEach.call( document.querySelectorAll('#id_phone'), function(input) {
-    var keyCode;
-    function mask(event) {
-        event.keyCode && (keyCode = event.keyCode);
-        var pos = this.selectionStart;
-        if (pos < 3) event.preventDefault();
-        var matrix = "+7 (___)-___-__-__",
-            i = 0,
-            def = matrix.replace(/\D/g, ""),
-            val = this.value.replace(/\D/g, ""),
-            new_value = matrix.replace(/[_\d]/g, function(a) {
-                return i < val.length ? val.charAt(i++) || def.charAt(i) : a
-            });
-        i = new_value.indexOf("_");
-        if (i != -1) {
-            i < 5 && (i = 3);
-            new_value = new_value.slice(0, i)
-        }
-        var reg = matrix.substr(0, this.value.length).replace(/_+/g,
-            function(a) {
-                return "\\d{1," + a.length + "}"
-            }).replace(/[+()]/g, "\\$&");
-        reg = new RegExp("^" + reg + "$");
-        if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
-        if (event.type == "blur" && this.value.length < 5)  this.value = ""
+document.addEventListener("DOMContentLoaded", function () {
+    let phoneInputs = document.querySelectorAll('#id_phone');
+
+    let getInputNumbersValue = function (input) {
+        // Возвращаем только цифры из введенного значения
+        return input.value.replace(/\D/g, '');
     }
 
-    input.addEventListener("input", mask, false);
-    input.addEventListener("focus", mask, false);
-    input.addEventListener("blur", mask, false);
-    input.addEventListener("keydown", mask, false)
+    let onPhonePaste = function (e) {
+        let input = e.target,
+            inputNumbersValue = getInputNumbersValue(input);
+        let pasted = e.clipboardData || window.clipboardData;
+        if (pasted) {
+            let regex = /\D/g;
+            let pastedText = pasted.getData('Text');
+            if (regex.test(pastedText)) {
+                // При вставке некорректного символа оставляем только цифры
+                input.value = inputNumbersValue;
+                return;
+            }
+        }
+    }
 
-  });
+    let onPhoneInput = function (e) {
+        let input = e.target,
+            inputNumbersValue = getInputNumbersValue(input),
+            selectionStart = input.selectionStart,
+            formattedInputValue = "";
 
+        if (!inputNumbersValue) {
+            return input.value = "";
+        }
+
+        if (input.value.length != selectionStart) {
+            // Если редактируем не в конце поля
+            if (e.data && /\D/g.test(e.data)) {
+                // Если вводим не числовой символ
+                input.value = inputNumbersValue;
+            }
+            return;
+        }
+
+        if (["7", "8", "9"].indexOf(inputNumbersValue[0]) > -1) {
+            if (inputNumbersValue[0] == "9") inputNumbersValue = "7" + inputNumbersValue;
+            let firstSymbols = (inputNumbersValue[0] == "8") ? "8" : "+7";
+            formattedInputValue = input.value = firstSymbols + " ";
+            if (inputNumbersValue.length > 1) {
+                formattedInputValue += '(' + inputNumbersValue.substring(1, 4);
+            }
+            if (inputNumbersValue.length >= 5) {
+                formattedInputValue += ') ' + inputNumbersValue.substring(4, 7);
+            }
+            if (inputNumbersValue.length >= 8) {
+                formattedInputValue += '-' + inputNumbersValue.substring(7, 9);
+            }
+            if (inputNumbersValue.length >= 10) {
+                formattedInputValue += '-' + inputNumbersValue.substring(9, 11);
+            }
+        } else {
+            formattedInputValue = '+' + inputNumbersValue.substring(0, 16);
+        }
+        input.value = formattedInputValue;
+    }
+
+    let onPhoneKeyDown = function (e) {
+        // Удаление последнего символа очищает поле
+        let inputValue = e.target.value.replace(/\D/g, '');
+        if (e.keyCode == 8 && inputValue.length == 1) {
+            e.target.value = "";
+        }
+    }
+
+    let onPhoneFocus = function (e) {
+        let input = e.target;
+        if (input.value === "") {
+            input.value = "+7 "; // Устанавливаем начальное значение
+            input.setSelectionRange(3, 3); // Устанавливаем курсор после +7
+        }
+    }
+
+    for (let phoneInput of phoneInputs) {
+        phoneInput.addEventListener('keydown', onPhoneKeyDown);
+        phoneInput.addEventListener('input', onPhoneInput, false);
+        phoneInput.addEventListener('paste', onPhonePaste, false);
+        phoneInput.addEventListener('focus', onPhoneFocus); // Добавляем обработчик focus
+    }
 });
